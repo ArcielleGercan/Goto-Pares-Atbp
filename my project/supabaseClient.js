810327fa19dad2af124ad05a3c6d9f4cf7b06427
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-02-22 15:08:33 +0000

    trying this api

--- my project/supabaseClient.js
+++ my project/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const SUPABASE_URL = "https://eiybmfozvyylfstdfmxx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpeWJtZm96dnl5bGZzdGRmbXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTg2MDksImV4cCI6MjA1NTc5NDYwOX0.bhVMBJVO5mYq5swQXb5c4T8tDepjNGG1P99o8UXVKvE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpeWJtZm96dnl5bGZzdGRmbXh4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDIxODYwOSwiZXhwIjoyMDU1Nzk0NjA5fQ.CrSYvys9PCS3vv3b_9XYSdPh4V5nf_VRmrgNk5PcVDs";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
